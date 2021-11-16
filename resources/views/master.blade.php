f8590540e8fc3b17e1ec55340bf124b511a84854
KingBoocin <adidananjaya34@gmail.com>
2021-11-16 22:38:26 +0800

    login done

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -48,6 +48,13 @@
         </div><!-- /.container-fluid -->
       </nav>
     @yield('content')
+    <!-- footer -->
+    <div class="panel panel-default">
+      <div class="panel-body">
+        Panel content
+      </div>
+      <div class="panel-footer">Panel footer</div>
+    </div>
 </body>
 <style>
     .custom-login{
